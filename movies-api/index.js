275d8311dbfef10704d305f9d596ba1472b3974c
Alexis <edwinalexis1996@gmail.com>
2020-07-05 20:43:32 -0500

    add-notes

---
+++ movies-api/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+
+const authApi = require('./routes/auth');
+const moviesApi = require('./routes/movies.js');
+const userMoviesApi = require('./routes/userMovies.js');
+
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// body parser
+app.use(express.json());
+
+// routes
+authApi(app);
+moviesApi(app);
+userMoviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
+});
